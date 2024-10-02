6801b7c9e7bee49f2e32f849eb947d224cd046aa
GurbetKasbas <gurbetkasbas@gmail.com>
2024-10-02 12:08:21 +0300

    Proje dosyalarını ekle

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:sayi_tahmin_uygulamasi/anasayfa.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+       scaffoldBackgroundColor: Colors.white,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Anasayfa(),
+    );
+  }
+}
+
