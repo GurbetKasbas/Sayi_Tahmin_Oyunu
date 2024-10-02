1936f8c10d9286d380c218fb69774ad4f601c6a4
GurbetKasbas <gurbetkasbas@gmail.com>
2024-10-02 12:14:00 +0300

    Anasayfa değişikliklerini ekle

--- lib/anasayfa.dart
+++ lib/anasayfa.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sayi_tahmin_uygulamasi/tahmin_ekrani.dart';
 
 class Anasayfa extends StatefulWidget {
   const Anasayfa({super.key});
@@ -15,7 +16,29 @@ class _AnasayfaState extends State<Anasayfa> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-           
+            //Başlık
+           const  Text("Tahmin Oyunu",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 5
+            ),
+            ),
+            //Görsel
+            Image.asset("assets/images/zar.jpg"),
+            //Button
+            SizedBox(
+              width: 300,
+              height: 50,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                ),
+                onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => const  TahminEkrani()));
+                }, 
+                child: const Text("Oyuna Başla" , style: TextStyle(color: Colors.white , fontSize: 20), )),
+            )
           ],
         ),
       ),
